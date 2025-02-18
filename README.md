3b9c291adc965a43bd6899a1def1e4c05698d5a0
Luisfer-Atom <luisfermotta24@gmail.com>
2025-02-18 01:47:56 -0600

    Proyecto Sprint 7 listo para revisión

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Proyecto Urban Grocers 
+# Descripción: Un equipo QA Engineer está comprobando cómo la aplicación Urban Grocers crea kits de productos.
+# En este proyecto se trabajará la lista de comprobación para el campo "name" en la solicitud de creación de un kit de productos.
+
+# Lista de comprobación:
+  1.- El número permitido de caracteres (1): kit_body = { "name": "a"}
+  2.- El número permitido de caracteres (511): kit_body = { "name":"El valor de prueba para esta comprobación será inferior a"}
+  3.- El número de caracteres es menor que la cantidad permitida (0): kit_body = { "name": "" }
+  4.- El número de caracteres es mayor que la cantidad permitida (512): kit_body = { "name":"El valor de prueba para esta comprobación será inferior a” }
+  5.- Se permiten caracteres especiales: kit_body = { "name": ""№%@"," }
+  6.- Se permiten espacios: kit_body = { "name": " A Aaa " }
+  7.- Se permiten números: kit_body = { "name": "123" }
+  8.- El parámetro no se pasa en la solicitud: kit_body = { }
+  9.- Se ha pasado un tipo de parámetro diferente (número): kit_body = { "name": 123 }
+
+# Pasos:
+# 1.- Iniciar el servidor y actualizar el URL en el archivo configuration.py
+# 2.- Abrir el archivo create_kit_name_kit_test.py
+# 3.- Correr cada una de las pruebas y comrpobar el resultado.
---
+++ data.py
@@ -0,0 +1,29 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Max",
+    "phone": "+10005553535",
+    "address": "8042 Lancaster Ave.Hamburg, NY"
+}
+
+kit_body = {
+    "name": "Mi Kit"
+}
+
+test1_kit_name = "a"
+test2_kit_name = "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC"
+test3_kit_name = ""
+test4_kit_name = "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD"
+test5_kit_name = "%@"#*"
+test6_kit_name = " A Aaa "
+test7_kit_name = "123"
+test9_kit_name = 123
+
+
+
+
+
+
+
