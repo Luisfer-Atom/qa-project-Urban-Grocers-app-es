5e414bba1f44b70eeca918c963f0d386c6257140
Luisfer-Atom <luisfermotta24@gmail.com>
2025-02-27 04:48:54 +0000

    Sprint 7 corregido para revisión

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-513a8557-b6f1-4e0e-a278-05c73ebcad9a.containerhub.tripleten-services.com"# Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-366eefd2-1143-453a-8f57-697e2d1b3064.containerhub.tripleten-services.com"# Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = "/api/v1/users"# Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = "/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
